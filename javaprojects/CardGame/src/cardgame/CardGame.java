eb7c7386b29d1f361d540384a39e33991c9b675f
Lenovo <Lenovo@LAPTOP-FLMGE6N0>
2022-02-01 20:46:13 -0500

    hi my name is akhil wadhera

--- javaprojects/CardGame/src/cardgame/CardGame.java
+++ javaprojects/CardGame/src/cardgame/CardGame.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package cardgame;
 
 /**
  *
- * @author Lenovo
+ * @author Akhil Wadhera
  */
 import java.util.Random;
 import java.util.Scanner;
