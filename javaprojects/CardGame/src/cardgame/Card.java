03b1e87b73f742b10cfb6b9bd55b21371acc6140
Lenovo <Lenovo@LAPTOP-FLMGE6N0>
2022-02-01 20:41:46 -0500

    hi my name is akhil wadhera

---
+++ javaprojects/CardGame/src/cardgame/Card.java
@@ -0,0 +1,35 @@
+package cardgame;
+/**
+ *
+ * @author Akhil Wadhera
+ */
+public class Card {
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
+
+    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "suit='" + suit + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ javaprojects/CardGame/src/cardgame/CardGame.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cardgame;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class CardGame {
+    public static void main(String[] args) {
+
+        Card[] magicHand = new Card[7];
+        Scanner scan = new Scanner(System.in);
+        Random generator = new Random();
+        generator.setSeed(System.currentTimeMillis());
+        System.out.println("The cards in the magic hand are: ");
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(generator.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[generator.nextInt(4)]);
+            magicHand[i] = c;
+            
+        }
+        
+         System.out.print("Enter card value: ");
+        int val = scan.nextInt();
+        System.out.print("Enter 1 for hearts, 2 for diamonds, 3 for spades, 4 for clubs");
+        
+        String suit = scan.next();
+        Card key = new Card();
+        key.setValue(val);
+        key.setSuit(suit);  
+        
+        Card c = new Card();
+        key.setValue(11);
+        key.setSuit("Hearts");
+        boolean found = false;
+        System.out.println("Searching for: " + key);
+        for (int i = 0; i < magicHand.length; i++) {
+            if ((magicHand[i].getValue() == key.getValue()) && magicHand[i].getSuit().equalsIgnoreCase(key.getSuit())) {
+                System.out.println("Card is found in magic hand at index " + (i+1));
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            System.out.println("Card is not found in magic hand.");
+    }
+}
